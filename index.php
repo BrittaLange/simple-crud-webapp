d6d1b6f040b1965f520bf14a2beb697e81e0ee7b
BrittaLange <britta.lange@mailbox.org>
2026-01-04 16:59:49 +0100

    Implemented update functionality Resolves #7

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 // Connect to database.
 $db = require_once  '../../config/connect.php';
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Variable declaration.
+$update = false;
+$id = -1;
 // Deleting a customer dataset.
 if (isset($_GET['delete'])) {
     try {
@@ -11,6 +14,23 @@ if (isset($_GET['delete'])) {
         echo $affectedRows . " datasets deleted.";
     } catch (PDOException $e) {
         echo "Deleting dataset failed. " . $e->getMessage();
+    }
+}
+// Start edit mode.
+if (isset($_GET['edit'])) {
+    try {
+        $update = true;
+        $id = $_GET['edit'];
+        $sql = "SELECT name, location FROM customers WHERE id = $id";
+        $result = $db->query($sql);
+        if ($result !== false) {
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                $name = $row['name'];
+                $location = $row['location'];
+            }
+        }
+    } catch (PDOException $e) {
+        echo "Editing failed. " . $e->getMessage();
     }
 }
 // Input Validations
@@ -52,6 +72,24 @@ if (isset($_POST['name'])) {
             echo "Saving data did not work: " . $e->getMessage();
         }
     }
+    // Updating a customer.
+    if (isset($_POST['update'])) {
+        try {
+            $id = $_POST['id'];
+            $sql = "UPDATE customers SET name = :name, location = :location WHERE id = :id";
+            $stmt = $db->prepare($sql);
+            $stmt->execute([
+                ':name' => $name,
+                ':location' => $location,
+                ':id' => $id
+            ]);
+            if ($stmt->rowCount() > 0) {
+                echo "Your changes have been saved.";
+            }
+        } catch (PDOException $e) {
+            echo "Update operation failed. " . $e->getMessage();
+        }
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -65,52 +103,70 @@ if (isset($_POST['name'])) {
 </head>
 
 <body>
-    <div class="m-5">
-        <legend>List of customers</legend>
-        <table class="table table-bordered table-striped table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Location</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                // Retrieve and display all customers in a table.
-                $sql = "SELECT id, name, location FROM customers";
-                foreach ($db->query($sql) as $row) {
-                    echo "<tr>";
-                    echo "<td>" . htmlspecialchars($row[1]) . "</td>";
-                    echo "<td>" . htmlspecialchars($row[2]) . "</td>";
-                    echo '<td>';
-                    echo '<a href="index.php?edit=';
-                    echo $row[0] . '" class="btn btn-info" type="button">Edit</a>';
-                    echo '<a href="index.php?delete=';
-                    echo $row[0] . '" class="btn btn-danger" type="button">Delete</a>';
-                    echo '</td>';
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="m-5">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-            <legend>Create new customer</legend>
-            <div class="mb-3">
-                <label for="inputName" class="form-label">Name</label>
-                <input type="text" name="name" class="form-control" id="inputName" maxlength="120">
-                <div id="nameRequired" class="form-text text-danger"><?= $errorMessage['name'] ?? ''; ?></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-8">
+                <legend>List of customers</legend>
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Location</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Retrieve and display all customers in a table.
+                        $sql = "SELECT id, name, location FROM customers";
+                        foreach ($db->query($sql) as $row) {
+                            echo "<tr>";
+                            echo "<td>" . htmlspecialchars($row[1]) . "</td>";
+                            echo "<td>" . htmlspecialchars($row[2]) . "</td>";
+                            echo '<td>';
+                            echo '<a href="index.php?edit=';
+                            echo $row[0] . '" class="btn btn-info me-2" type="button">Edit</a>';
+                            echo '<a href="index.php?delete=';
+                            echo $row[0] . '" class="btn btn-danger" type="button">Delete</a>';
+                            echo '</td>';
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </div>
-            <div class="mb-3">
-                <label for="inputLocation" class="form-label">Location</label>
-                <input type="text" name="location" class="form-control" id="inputLocation" maxlength="120">
-                <div id="locationRequired" class="form-text text-danger"><?= $errorMessage['location'] ?? ''; ?></div>
-            </div>
-            <button type="submit" class="btn btn-primary" name="save">Save</button>
-        </form>
+        </div>
     </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-8">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                    <?php if ($update == true): ?>
+                        <legend>Edit customer</legend>
+                    <?php else: ?>
+                        <legend>Create new customer</legend>
+                    <?php endif; ?>
+                    <input type="text" hidden name="id" value="<?php echo $id ?>">
+                    <div class="mb-3">
+                        <label for="inputName" class="form-label">Name</label>
+                        <input type="text" name="name" class="form-control" id="inputName" maxlength="120" value=<?= $name ?? '' ?>>
+                        <div id="nameRequired" class="form-text text-danger"><?= $errorMessage['name'] ?? ''; ?></div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="inputLocation" class="form-label">Location</label>
+                        <input type="text" name="location" class="form-control" id="inputLocation" maxlength="120" value=<?= $location ?? '' ?>>
+                        <div id="locationRequired" class="form-text text-danger"><?= $errorMessage['location'] ?? ''; ?></div>
+                    </div>
+                    <?php if ($update == true): ?>
+                        <button type="submit" class="btn btn-info" name="update">Update</button>
+                    <?php else: ?>
+                        <button type="submit" class="btn btn-primary" name="save">Save</button>
+                    <?php endif; ?>
+                </form>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
